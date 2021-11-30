ade19b7b8993bf05b49ac21252f016fb185eb0cf
Konstantin Rybakov <viocost@gmail.com>
2021-11-30 20:02:47 +0500

    Adding types: work in progress

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,28 +1,37 @@
-export interface ICuteSet<T> {
-  forEach(cb: Function, thisArg?: any): void;
-  filter(cb: Function, thisArg?: any): ICuteSet<T>;
-  map(cb: Function): ICuteSet<T>;
-  reduce(cb: Function, initialValue?: any): any;
-  union<TInput = any>(input: TInput): ICuteSet<T | TInput>;
-  minus<TInput = any>(input: TInput): ICuteSet<T | TInput>;
-  complement<TInput = any>(input: TInput): ICuteSet<T | TInput>;
-  symmetricDifference<TInput = any>(input: TInput): ICuteSet<T | TInput>;
-  intersection<TInput = any>(input: TInput): ICuteSet<T | TInput>;
-  isEqual<TInput = any>(input: TInput): boolean;
-  isSubsetOf<TInput = any>(input: TInput): boolean;
-  sort(fn: Function): ICuteSet<T>;
-  powerSet(): ICuteSet<T>;
-  subsets(): Generator<CuteSet<T[]>, void, unknown>;
-  permutations(): Generator<CuteSet<T[]>, void, unknown>;
-  has(candidate: T): boolean;
-  add(candidate: T): void;
-  remove(candidate: T): void;
+export interface ICuteSet<T = any> {
+  union(input: TInput): ICuteSet<T | TInput>; //
   toArray(): Array<T>;
-  toString(delimiter: string): string;
+  has(candidate: T): boolean; //
 
-  print(delimiter: string): void;
+  readonly length: number;
+  readonly [n: number]: T;
+  /////////////////////////////////////////////////////////////////////////////
+  // forEach(cb: Function, thisArg?: any): void;                             //
+  // filter(cb: Function, thisArg?: any): ICuteSet<T>;                       //
+  // map(cb: Function): ICuteSet<T>;                                         //
+  // reduce(cb: Function, initialValue?: any): any;                          //
+  // union<TInput = any>(input: TInput): ICuteSet<T | TInput>;               //
+  // minus<TInput = any>(input: TInput): ICuteSet<T | TInput>;               //
+  // complement<TInput = any>(input: TInput): ICuteSet<T | TInput>;          //
+  // symmetricDifference<TInput = any>(input: TInput): ICuteSet<T | TInput>; //
+  // intersection<TInput = any>(input: TInput): ICuteSet<T | TInput>;        //
+  // isEqual<TInput = any>(input: TInput): boolean;                          //
+  // isSubsetOf<TInput = any>(input: TInput): boolean;                       //
+  // sort(fn: Function): ICuteSet<T>;                                        //
+  // powerSet(): ICuteSet<T>;                                                //
+  // subsets(): Generator<CuteSet<T[]>, void, unknown>;                      //
+  // permutations(): Generator<CuteSet<T[]>, void, unknown>;                 //
+  // add(candidate: T): void;                                                //
+  // remove(candidate: T): void;                                             //
+  // toArray(): Array<T>;                                                    //
+  // toString(delimiter: string): string;                                    //
+  //                                                                         //
+  // print(delimiter: string): void;                                         //
+  /////////////////////////////////////////////////////////////////////////////
 }
 
-export interface ICallback {
-  (value, index, array): ICuteSet;
+export type CuteSetInput<T = any> = T | ICuteSet<T> | ArrayLike<T>;
+
+export interface ICallback<T = any> {
+  (value, index, array): T;
 }
